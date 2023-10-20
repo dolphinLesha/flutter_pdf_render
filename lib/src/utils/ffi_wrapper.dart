20dd484b093cec46c5c09c47d2bd448e1b3f0487
Alexey Dybko <adybko@ab-technology.ru>
2023-10-20 09:35:48 +0300

    update imports

--- lib/src/utils/ffi_wrapper.dart
+++ lib/src/utils/ffi_wrapper.dart
@@ -1,2 +1,2 @@
 // This file is used to switch dart:ffi import to dummy import for Flutter Web
-export 'dart:ffi' if (dart.library.js) 'web_pointer.dart';
+export 'exports/export_dart_ffi.dart' if (dart.library.js) 'web_pointer.dart';
